1f10d0eccde56f490eb79cfa0b68918c7c70da8b
Gameboy612 <64597724+Gameboy612@users.noreply.github.com>
2023-12-09 23:26:21 +0800

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+def this_is_a_function(x: int, y: int) -> int:
+    """Returns added values.
+
+    Args:
+        x (int): x
+        y (int): y
+
+    Returns:
+        int: output
+    """
+    return x + y
